5372453d660e4fef70d37889c08479aab720dd0c
kazukiiobuild <acipkzn09609@outlook.com>
2025-12-26 10:23:29 +0800

    deploy: register EtherealMint deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployed = await deploy("EtherealMint", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EtherealMint contract: ${deployed.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_etherealMint"; // id required to prevent reexecution
+func.tags = ["EtherealMint"];
